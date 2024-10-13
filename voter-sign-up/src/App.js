609e139753baa920580eebe0f5b27b6b9c6a2bd2
Harshank Patel <astroid169@gmail.com>
2024-10-13 04:07:50 -0500

    Added footer

--- voter-sign-up/src/App.js
+++ voter-sign-up/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import VoterForm from './component/VoterForm';
+import FooterForm from './component/FooterForm';
 
 function App() {
   return (
     <div className="App">
       <VoterForm />
+      <FooterForm />
     </div>
   );
 }
